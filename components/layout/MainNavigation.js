44c619667fde7e4515aff04612e5a0807687faa1
Colin Richardson <richardsonjcolin@gmail.com>
2021-11-26 13:18:41 -0600

    include nextjs link; add Link elements for '/' and '/new-meetup'

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,4 +1,4 @@
-// import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 import classes from './MainNavigation.module.css';
 
@@ -8,8 +8,12 @@ function MainNavigation() {
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
-          <li>{/* <Link to='/'>All Meetups</Link> */}</li>
-          <li>{/* <Link to='/new-meetup'>Add New Meetup</Link> */}</li>
+          <li>
+            <Link href='/'>All Meetups</Link>
+          </li>
+          <li>
+            <Link href='/new-meetup'>Add New Meetup</Link>
+          </li>
         </ul>
       </nav>
     </header>
