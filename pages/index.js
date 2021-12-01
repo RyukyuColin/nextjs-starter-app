c4ce2150b04dde04eae03e8536f2bada4893e4de
Colin Richardson <richardsonjcolin@gmail.com>
2021-12-01 17:59:10 -0600

    remove mock data and fetch meetups from DB client

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,36 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 
-const MOCK_MEETUPS = [
-  {
-    id: 1,
-    title: 'First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Istana_%28Singapore%29.jpg/1280px-Istana_%28Singapore%29.jpg',
-    address: '123 Lane Way, Singapore 12345',
-    description: 'This is our very first meetup!',
-  },
-  {
-    id: 2,
-    title: 'Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Singapore_Merlion-at-Marina-Bay-01.jpg/1280px-Singapore_Merlion-at-Marina-Bay-01.jpg',
-    address: '4th Street N, Singapore 98765',
-    description: "This is our second meetup. It just keeps gettin' better!",
-  },
-];
+const MONGODB_PW = process.env['MONGODB_PW'];
 
-function HomePage() {
-  return <MeetupList meetups={MOCK_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
 
+export async function getStaticProps() {
+  // connect to DB
+  const client = await MongoClient.connect(
+    `mongodb+srv://admin-demo:${MONGODB_PW}@nextjs-demo.iflv5.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  // get collection
+  const meetupsCollection = db.collection('meetups');
+  // fetch meetups
+  const meetups = await meetupsCollection.find().toArray();
+
+  // close db connection
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
+    },
+    revalidate: 10,
+  };
+}
 export default HomePage;
