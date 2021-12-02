9bfc1b544b6e995f9dce5109b5ca572f92fa6ef6
Colin Richardson <richardsonjcolin@gmail.com>
2021-12-01 18:01:03 -0600

    handle form submit and add fetch for POST /api/new-meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,24 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
-function NewMeetupPage(params) {
-  const addMeetupHandler = (meetupData) => {
-    console.log(meetupData);
-  };
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(meetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    const data = await response.json();
+
+    console.log(data);
+
+    // redirect to index
+    router.push('/');
+  }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
