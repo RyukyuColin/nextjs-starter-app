1aa04b2b4227a8547113e40fd2e52cbcae5b0766
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-02 17:11:14 -0600

    fetch all meetup ids in getStaticPaths for path generation

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,29 @@
+import meetupModel from '../api/src/models/meetup';
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 function MeetupDetailsPage() {
-  return (
-    <>
-      <img src='' alt='' />
-    </>
-  );
+  return <MeetupDetail />;
 }
 
+export async function getStaticPaths() {
+  const meetups = await meetupModel.fetchAll({}, { _id: 1 });
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+export async function getStaticProps(context) {
+  // fetch data for single meetup
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {},
+    },
+  };
+}
 export default MeetupDetailsPage;
