d8768e8f74a1bf8a488c4294df19724ef6116057
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-03 16:42:10 -0600

    update env variables

--- next.config.js
+++ next.config.js
@@ -4,20 +4,16 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: 'maximilian',
-        mongodb_password: '2YkcXq43KyPk0vqp',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site-dev',
+        DB_URI: `mongodb+srv://${process.env['DB_USER']}:${process.env['DB_PASSWORD']}${process.env['DB_HOST']}/${process.env['DB_NAME']}?${process.env['DB_OPTIONS']}`,
+        DB_NAME: 'meetups',
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
+      DB_URI: `mongodb+srv://${process.env['DB_USER']}:${process.env['DB_PASSWORD']}${process.env['DB_HOST']}/${process.env['DB_NAME']}?${process.env['DB_OPTIONS']}`,
+      DB_NAME: 'meetups',
     },
   };
 };
