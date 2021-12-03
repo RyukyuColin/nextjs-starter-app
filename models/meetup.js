64ea74a91dff6a7e7a450b05cebb79b3662a6ba8
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-03 12:46:28 -0600

    refactor to deserialize mongo db and remove client.close in place of useUnifiedTopology option in client initialization

--- models/meetup.js
+++ models/meetup.js
@@ -1,31 +1,26 @@
-import mongoClient from '../lib/db/index';
+// import mongoClient from '../lib/db/index';
+import connectToDb from '../lib/db/index';
 
 const COLLECTION = 'meetups';
 
 const fetchOne = async (query = {}, options = {}) => {
-  const client = await mongoClient();
-  const db = client.db();
+  const { db } = await connectToDb();
   const collection = db.collection(COLLECTION);
   const result = await collection.findOne(query, options);
-  client.close();
   return result;
 };
 
 const fetchAll = async (query = {}, options = {}) => {
-  const client = await mongoClient();
-  const db = client.db();
+  const { db } = await connectToDb();
   const collection = db.collection(COLLECTION);
   const result = await collection.find(query, options).toArray();
-  client.close();
   return result;
 };
 
 const createOne = async (data) => {
-  const client = await mongoClient();
-  const db = client.db();
+  const { db } = await connectToDb();
   const collection = db.collection(COLLECTION);
   const result = await collection.insertOne(data);
-  client.close();
   return result;
 };
 
